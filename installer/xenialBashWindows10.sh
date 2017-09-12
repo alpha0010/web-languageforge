f3c90178a415d5f89825391285a7b9776fc9f937
megahirt <chris@hirtfamily.net>
2017-09-12 10:48:45 +0700

    more installer changes

--- installer/xenialBashWindows10.sh
+++ installer/xenialBashWindows10.sh
@@ -1,55 +1,74 @@
-#!/bin/bash -x
+#!/bin/bash
+
+if ! [ `whoami` == "root" ]
+then
+	echo This script must be run with sudo!
+	exit
+fi
+
 echo -e "
-Notice: This installer assumes the following:\n\n
-- it is being run on a fresh Ubuntu Xenial on Windows installation.\n
-- it is being run from a source directory (e.g. /mnt/c/src ) where the web-languageforge repo will be cloned.\n
-\n
-  If this is not the case, continue at your own risk\n
-\n
-In the off chance that you want to completely remove and reinstall Xenial Bash on Windows 10,\n
- you can run these commands before running this script:\n
- \n
-lxrun /uninstall /full \n
-lxrun /install \n
-\n\n"
+Notice: This installer assumes the following:
+
+- it is being run in Bash a fresh Ubuntu Xenial on Windows 10 installation.
+- it is being run from a source directory (e.g. /mnt/c/src ) where the web-languageforge repo will be cloned.
+
+  If this is not the case, continue at your own risk
+
+In the off chance that you want to completely remove and reinstall Xenial Bash on Windows 10,
+you can run these commands from a Windows Command Prompt (not Bash!) before running this script:
+
+lxrun /uninstall /full 
+lxrun /install 
+"
 
 read -n 1 -s -r -p "Press any key to start the xForge developer environment install process"
-sudo echo -e "\n\n"
+echo -e "\n\n"
 
-echo "Updating / Upgrading with apt"
-wget -O- http://linux.lsdev.sil.org/downloads/sil-testing.gpg | sudo apt-key add -
-sudo add-apt-repository -y 'deb http://linux.lsdev.sil.org/ubuntu xenial main'
-sudo add-apt-repository -y 'deb http://linux.lsdev.sil.org/ubuntu xenial-experimental main'
-sudo add-apt-repository -y ppa:ansible/ansible
-sudo apt update && sudo apt -y upgrade
+echo Add extra apt repositories
+wget -O- http://linux.lsdev.sil.org/downloads/sil-testing.gpg | apt-key add -
+add-apt-repository -y 'deb http://linux.lsdev.sil.org/ubuntu xenial main'
+add-apt-repository -y 'deb http://linux.lsdev.sil.org/ubuntu xenial-experimental main'
+add-apt-repository -y ppa:ansible/ansible
 
-echo "Install NodeJS 8.X and latest npm"
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
+echo Install NodeJS 8.X and latest npm
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install -y nodejs
 
-echo "Installing packages"
-sudo apt install -y git ansible php7.0-cli libapache2-mod-php mongodb-server p7zip-full php7.0-dev php7.0-gd php7.0-intl php7.0-mbstring php-pear php-xdebug postfix unzip lfmerge
+echo Install postfix non-interactively
+DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
 
-echo "Cloning web-languageforge repo into the current directory... Is this what you want to do?  (If not, Ctrl-C and change this script)"
-read -n 1 -s -r -p "Press any key to continue"
-git clone --recurse-submodules https://github.com/sillsdev/web-languageforge
-cd web-languageforge\deploy
+echo Install and upgrade packages
+apt install -y git ansible php7.0-cli libapache2-mod-php mongodb-server p7zip-full php7.0-dev php7.0-gd php7.0-intl php7.0-mbstring php-pear php-xdebug postfix unzip lfmerge
+apt -y upgrade
 
-echo "Running xforge web developer ansible scripts"
-ansible-playbook -i hosts playbook_create_config.yml --limit localhost -K
-ansible-playbook -i hosts playbook_webdeveloper_bash_windows10.yml --limit localhost -K
+if [ ! -d "web-languageforge/deploy" ]
+then
+	echo Clone web-languageforge repo into the current directory
+	git clone --recurse-submodules https://github.com/sillsdev/web-languageforge
+fi
 
-echo "Refreshing xForge dependencies"
+cd web-languageforge/deploy
+
+echo "Run xforge web developer ansible scripts"
+ansible-playbook -i hosts playbook_create_config.yml --limit localhost
+ansible-playbook -i hosts playbook_webdeveloper_bash_windows10.yml --limit localhost
+
+echo "Refresh xForge dependencies"
 cd ..
 ./refreshDeps.sh
 
-echo "Modifying windows hosts file"
-echo -e "\n127.0.0.1\tlanguageforge.local\n" >> /mnt/c/Windows/System32/drivers/etc/hosts
-echo -e "\n127.0.0.1\tscriptureforge.local\n" >> /mnt/c/Windows/System32/drivers/etc/hosts
-echo -e "\n127.0.0.1\tjamaicanpsalms.scriptureforge.local\n" >> /mnt/c/Windows/System32/drivers/etc/hosts
-
-echo "Factory Reset the database"
+echo Factory Reset the database
 cd scripts/tools
 php FactoryReset.php run
 
-echo "You should now be able to access Language Forge locally at http://languageforge.local"
+HOSTSFILE=/mnt/c/Windows/System32/drivers/etc/hosts
+if [ -f "$HOSTSFILE" ]
+then
+	echo "Modify windows hosts file"
+	echo -e "\n127.0.0.1\tlanguageforge.local\n" >> $HOSTSFILE
+	echo -e "\n127.0.0.1\tscriptureforge.local\n" >> $HOSTSFILE
+	echo -e "\n127.0.0.1\tjamaicanpsalms.scriptureforge.local\n" >> $HOSTSFILE
+fi
+
+echo You should now be able to access Language Forge locally at http://languageforge.local
+echo Installation finished!
